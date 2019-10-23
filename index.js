148fa98e835effe3f8c6cb057c067cbc53648c50
Shahad A. Bagarish <shahad.bagarish@gmail.com>
2019-10-23 12:45:14 +0300

    test

---
+++ index.js
@@ -0,0 +1,6 @@
+let test = "hi"
+let name = require("./shahad")
+
+console.log(test);
+name()
+
---
+++ shahad.js
@@ -0,0 +1,5 @@
+function name() {
+    console.log("I am in shahad.js file");
+}
+
+module.exports = name
